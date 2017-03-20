92905e20961f1af0b93d4859d46f6a803ade1ade
LinearLaw <m18720991459_1@163.com>
2017-03-21 01:09:00 +0800

    数据绑定基本完成，总价需要调试

--- js/all_goods.js
+++ js/all_goods.js
@@ -1,29 +1,60 @@
 /**
  * Created by Administrator on 2017/3/20.
  */
-angular.module("AGoods",[])
-    .controller("AGoods_Ctrl",["$scope", function ($scope) {
-        $scope.list=[{
-            shopName:"新生格度",
-            imgSrc:"../images/tb20.jpg",
-            goTitle:"春季衬衫男长袖修身韩版加绒青年纯色男士休闲牛津纺白衬衣男装潮",
-            goType:"颜色：【无绒】浅海蓝,尺码：L",
-            price:57.00,
-            count:1,
+angular.module("AGoods", [])
+    .controller("AGoods_Ctrl", ["$scope", function ($scope) {
+        $scope.list = [{
+            shopName: "新生格度",
+            imgSrc: "../images/tb20.jpg",
+            goTitle: "春季衬衫男长袖修身韩版加绒青年纯色男士休闲牛津纺白衬衣男装潮",
+            goType: "颜色：【无绒】浅海蓝,尺码：L",
+            price: 57.00,
+            count: 1,
+            checkitem:true,
             minus: function () {
-                if(this.count==0){
+                if (this.count == 0) {
                     return;
-                }else{
+                } else {
                     this.count--;
                 }
             },
             plus: function () {
-                if(this.count>20){
+                if (this.count > 20) {
                     return;
                 }
-                else{
+                else {
+                    this.count++;
+                }
+            }
+        }, {
+            shopName: "新生格度",
+            imgSrc: "../images/tb20.jpg",
+            goTitle: "春季衬衫男长袖修身韩版加绒青年纯色男士休闲牛津纺白衬衣男装潮",
+            goType: "颜色：【无绒】浅海蓝,尺码：L",
+            price: 57.00,
+            count: 1,
+            checkitem:false,
+            minus: function () {
+                if (this.count == 0) {
+                    return;
+                } else {
+                    this.count--;
+                }
+            },
+            plus: function () {
+                if (this.count > 20) {
+                    return;
+                }
+                else {
                     this.count++;
                 }
             }
         }]
+        var sum=0;
+        angular.forEach($scope.list, function (value, key) {
+            if(key.checkitem){
+                sum += key.count * key.price;
+            }
+        })
+        $scope.totalPrice=sum;
     }])
