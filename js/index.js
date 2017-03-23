11715c685b60e45780179f6b7ae2329fac0e5325
LinearLaw <m18720991459_1@163.com>
2017-03-23 23:19:35 +0800

    完成了顶部导航栏

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2017/3/20.
  */
-angular.module("cart",["ui.router","AGoods"])
+angular.module("cart",["ui.router","AGoods","nav"])
     .controller("cartCtrl",["$scope", function ($scope) {
 
     }])
@@ -9,8 +9,15 @@ angular.module("cart",["ui.router","AGoods"])
         //1、全部商品模块配置
         $stateProvider.state({
             url:"/agoods",
-            name:"agoods",
+            name:"nav.agoods",
             templateUrl:"../pages/all_goods.html",
             controller:"AGoods_Ctrl"
+        })
+        //2、顶部导航栏设置
+        $stateProvider.state({
+            url:"/nav",
+            name:"nav",
+            templateUrl:"../pages/nav.html",
+            controller:"nav_Ctrl"
         })
     }])
---
+++ js/nav.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2017/3/23.
+ */
+angular.module("nav",[])
+    .controller("nav_Ctrl",["$scope", function ($scope) {
+        $scope.username="tb9999_20xx";
+    }])
