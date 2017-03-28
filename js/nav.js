fe4948799262968be0b5fcb7d41d6a2a359515a0
LinearLaw <m18720991459_1@163.com>
2017-03-28 11:24:11 +0800

    完成了底部footer模块

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,38 @@
 /**
  * Created by Administrator on 2017/3/23.
  */
-angular.module("nav",[])
-    .controller("nav_Ctrl",["$scope", function ($scope) {
-        $scope.username="tb9999_20xx";
+angular.module("nav", [])
+    .controller("nav_Ctrl", ["$scope", "$http", function ($scope, $http) {
+        $scope.username = "tb9999_20xx";
+        $scope.service = null;
+        $scope.navinfo = null;
+        $scope.copys = null;
+
+        /**
+         * @desc 模拟ajax获取服务商的json信息
+         */
+        $http({
+            url: "../data/service.json"
+        }).then(function (res) {
+            //console.log(res);
+            $scope.service = res.data;
+        })
+
+        /**
+         * @desc 模拟ajax获取关于淘宝的信息
+         */
+        $http({
+            url: "../data/navinfo.json"
+        }).then(function (res) {
+            $scope.navinfo = res.data;
+        })
+
+        /**
+         * @desc 获取淘宝的许可证信息
+         */
+        $http({
+            url: "../data/copys.json"
+        }).then(function (res) {
+            $scope.copys = res.data;
+        })
     }])
