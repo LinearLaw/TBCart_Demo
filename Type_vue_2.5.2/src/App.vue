ed4af01fa3e1a910370a4951e48fce390057c687
USERCHI-QIG9P26\Administrator <m18720991459_1@163.com>
2017-06-23 20:59:15 +0800

    update header tab css

--- Type_vue_2.5.2/src/App.vue
+++ Type_vue_2.5.2/src/App.vue
@@ -128,5 +128,40 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-
+//1、顶部导航栏
+#nav {
+  > #TB_nav {
+    width: 100%;
+    height: 35px;
+    background-color: rgb(245, 245, 245);
+    border-bottom: rgb(238, 238, 238);
+    >.container{
+      >ul{
+        >li{
+          float: left;
+          >a{
+            cursor: pointer;
+            font:12px/35px tahoma,arial,"Hiragino Sans GB","宋体",sans-serif;
+            &:hover{
+              color:#ff4400;
+            }
+          }
+        }
+        >.otherColor{
+          color:rgb(108, 108, 108);
+        }
+      }
+      >.left_Nav{
+        >li{
+          margin-right:20px;
+        }
+      }
+      >.right_nav{
+        >li{
+          margin-left:20px;
+        }
+      }
+    }
+  }
+}
 </style>
