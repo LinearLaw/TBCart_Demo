9a9e5dc5abded78975bc15466c13976bbfb16070
USERCHI-QIG9P26\Administrator <m18720991459_1@163.com>
2017-06-24 21:01:00 +0800

    Update logo

--- Type_vue_2.5.2/src/App.vue
+++ Type_vue_2.5.2/src/App.vue
@@ -164,4 +164,58 @@ export default {
     }
   }
 }
+
+//2、LOGO部分
+#logo {
+  > .container {
+    height: 98px;
+    padding: 20px 0;
+    >.head_left_logo{
+      >.tb_title{
+        position: absolute;
+        opacity:0;
+      }
+      >.logo{
+        width: 105px;
+        height: 45px;
+        background-size:300px 225px;
+        background-image: url("./assets/img/logo.png");
+        background-position:  -150px -150px;
+        vertical-align: middle;
+      }
+      >h1{
+        margin-left: 12px;
+        font-size:22px;
+        color:rgb(60, 60, 60);
+        line-height: 45px;
+      }
+    }
+    >.head_right_search{
+      width: 510px;
+      border:3px solid #ff4400;
+      select,button,input{
+        border:0;
+        outline:0;
+      }
+      >button{
+        width: 90px;
+        height: 30px;
+        float: right;
+        background-color: #ff4400;
+        font:18px/30px "Microsoft YaHei";
+        color:#fff;
+        &:hover{
+          background-color: #F52B00;
+        }
+      }
+      >select{
+        width: 75px;
+        height:30px;
+        padding-left:15px;
+        background-color: rgb(245,245,245);
+      }
+    }
+  }
+}
+
 </style>
