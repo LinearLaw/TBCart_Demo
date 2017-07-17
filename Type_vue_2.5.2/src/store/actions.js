b278b4c2642c824e9eff8ff8d5ad619eafccf84c
LinearLiu <linear.liu@cloudaxis.com>
2017-07-17 12:55:13 +0800

    Fix bug of actions

--- Type_vue_2.5.2/src/store/actions.js
+++ Type_vue_2.5.2/src/store/actions.js
@@ -2,13 +2,6 @@
 import * as types from './mutation-types'
 import axios from "axios"
 
-export const addToCart = ({ commit }, product) => {
-  if (product.inventory > 0) {
-    commit(types.ADD_TO_CART, {
-      id: product.id
-    })
-  }
-}
 export const getServiceList = ({commit})=>{
   axios.get("/static/json/service.json").then((result)=>{
     commit("GET_SERVICE_LIST",result.data);
