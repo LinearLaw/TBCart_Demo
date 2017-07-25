08bd7a57ed8205138f39af0a13ae7cd7e41a38a5
LinearLiu <linear.liu@cloudaxis.com>
2017-07-25 17:01:24 +0800

    Fix bug of get data

--- Type_vue_2.5.2/src/store/actions.js
+++ Type_vue_2.5.2/src/store/actions.js
@@ -4,22 +4,26 @@ import axios from "axios"
 
 export const getServiceList = ({commit})=>{
   axios.get("/static/json/service.json").then((result)=>{
-    commit("GET_SERVICE_LIST",result.data);
+    let tempData = result.data;
+    commit("GET_SERVICE_LIST",tempData);
   })
 }
 export const getInfosList = ({commit})=>{
   axios.get("/static/json/navinfo.json").then((result)=>{
-    commit("GET_INFO_LIST",result.data);
+    let tempData = result.data;
+    commit("GET_INFO_LIST",tempData);
   })
 }
 export const getAboutList = ({commit})=>{
   axios.get("/static/json/copys.json").then((result)=>{
-    commit("GET_ABOUT_LIST",result.data);
+    let tempData = result.data;
+    commit("GET_ABOUT_LIST",tempData);
   })
 }
 export const getCartList = ({commit})=>{
   axios.get("/static/json/cart.json").then((result)=>{
-    commit("GET_CART_LIST",result.data);
+    let tempData = result.data;
+    commit("GET_CART_LIST",tempData);
   })
 }
 
