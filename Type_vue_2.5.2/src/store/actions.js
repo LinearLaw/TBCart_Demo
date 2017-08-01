d3b6bfb0b0dbbd30718144acaa5e5068d391b4d2
LinearLiu <linear.liu@cloudaxis.com>
2017-08-01 10:55:00 +0800

    Fix bug of dist

--- Type_vue_2.5.2/src/store/actions.js
+++ Type_vue_2.5.2/src/store/actions.js
@@ -3,25 +3,25 @@ import * as types from './mutation-types'
 import axios from "axios"
 
 export const getServiceList = ({commit})=>{
-  axios.get("/static/json/service.json").then((result)=>{
+  axios.get("../../static/json/service.json").then((result)=>{
     let tempData = result.data;
     commit("GET_SERVICE_LIST",tempData);
   })
 }
 export const getInfosList = ({commit})=>{
-  axios.get("/static/json/navinfo.json").then((result)=>{
+  axios.get("../../static/json/navinfo.json").then((result)=>{
     let tempData = result.data;
     commit("GET_INFO_LIST",tempData);
   })
 }
 export const getAboutList = ({commit})=>{
-  axios.get("/static/json/copys.json").then((result)=>{
+  axios.get("../../static/json/copys.json").then((result)=>{
     let tempData = result.data;
     commit("GET_ABOUT_LIST",tempData);
   })
 }
 export const getCartList = ({commit})=>{
-  axios.get("/static/json/cart.json").then((result)=>{
+  axios.get("../../static/json/cart.json").then((result)=>{
     let tempData = result.data;
     commit("GET_CART_LIST",tempData);
   })
